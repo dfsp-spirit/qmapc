ebc8f321a96b57078ce24dc8732f0063ef2d4e49
dfsp-spirit <ts+code@rcmd.org>
2017-05-24 00:05:21 +0100

    Update comments in all file headers.

--- src/main/java/org/rcmd/qmapc/parsing/parser/Quake2MapParser.java
+++ src/main/java/org/rcmd/qmapc/parsing/parser/Quake2MapParser.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This file is part of qmapc. See the LICENSE file for license information.
  */
 package org.rcmd.qmapc.parsing.parser;
 
