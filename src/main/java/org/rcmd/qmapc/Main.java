8e5330c3cb1bca7d6f553470386d55c13cf1e920
dfsp-spirit <ts+code@rcmd.org>
2017-05-30 23:16:59 +0100

    Work on parsing command line arguments.

--- src/main/java/org/rcmd/qmapc/Main.java
+++ src/main/java/org/rcmd/qmapc/Main.java
@@ -3,9 +3,6 @@
  */
 package org.rcmd.qmapc;
 
-import org.rcmd.qmapc.parsing.lexer.Lexer;
-import org.rcmd.qmapc.parsing.lexer.Quake2MapLexer;
-import org.rcmd.qmapc.parsing.lexer.Token;
 
 /**
  *
@@ -16,27 +13,8 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-
-        Quake2MapLexer q2ml;
-        String q2Brush;
-
-        q2Brush = "// brush 0\n"
-                + "{\n"
-                + "( 248 320 192 ) ( 248 -128 192 ) ( -136 320 192 ) spirit2dm9/floor_3 0 0 0 1.000000 1.000000 0 1 50\n"
-                + "( -128 312 192 ) ( -128 312 64 ) ( -128 -136 192 ) spirit2dm9/trim_9 0 0 0 1.000000 1.000000 0 1 50\n"
-                + "( -128 -128 160 ) ( 256 -128 160 ) ( -128 320 160 ) spirit2dm9/trim_9 0 0 0 1.000000 1.000000 0 1 50\n"
-                + "( -136 -128 64 ) ( -136 -128 192 ) ( 248 -128 64 ) spirit2dm9/trim_9 0 0 0 1.000000 1.000000 0 1 50\n"
-                + "( -192 -120 64 ) ( -192 328 64 ) ( -192 -120 192 ) spirit2dm9/trim_9 0 0 0 1.000000 1.000000 0 1 50\n"
-                + "( -192 0 -96 ) ( -128 -64 -96 ) ( -192 0 32 ) spirit2dm9/trim_9 0 0 0 1.000000 1.000000 0 1 50\n"
-                + "}";
-
-        q2ml = new Quake2MapLexer(q2Brush);
-        Token token = q2ml.nextToken();
-        while (token.type != Lexer.EOF_TYPE) {            
-            System.out.println("Received token '" + token + "'.");
-            token = q2ml.nextToken();
-        }
+    public static void main(String[] args) {        
+        new QmapcCLI(args).parse();
     }
 
 }
---
+++ src/main/java/org/rcmd/qmapc/QmapcCLI.java
@@ -0,0 +1,90 @@
+package org.rcmd.qmapc;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class QmapcCLI {
+
+    private static final Logger LOGGER = Logger.getLogger(QmapcCLI.class.getName());
+    private String[] args = null;
+    private final Options options = new Options();
+
+    public QmapcCLI(String[] args) {
+
+        this.args = args;
+
+        options.addOption("h", "help", false, "show help and exit.");
+        options.addOption("v", "version", false, "show version info and exit.");
+        options.addOption("a", "allow-overwrite", false, "overwrite existing output files.");
+        options.addOption(Option.builder("i")
+                .longOpt("input-map")
+                .desc("use input map file INMAP")
+                .hasArg()
+                .argName("INMAP")
+                .build());
+        options.addOption(Option.builder("o")
+                .longOpt("output-map")
+                .desc("use output map file OUTMAP")
+                .hasArg()
+                .argName("OUTMAP")
+                .build());
+        options.addOption(Option.builder("f")
+                .longOpt("infile-format")
+                .desc("use input map format FORMAT (one of: q1, q2, q3)")
+                .hasArg()
+                .argName("FORMAT")
+                .build());
+        options.addOption(Option.builder("F")
+                .longOpt("outfile-format")
+                .desc("use output map format FORMAT (one of: q1, q2, q3)")
+                .hasArg()
+                .argName("FORMAT")
+                .build());
+
+    }
+
+    public void parse() {
+        CommandLineParser parser = new DefaultParser();
+
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args);
+
+            if (cmd.hasOption("h")) {
+                help(0);
+            }
+
+            if (cmd.hasOption("v")) {
+                System.out.println("0.01");
+                System.exit(0);
+            }
+
+            if (cmd.hasOption("i")) {
+                String inputMapFile = cmd.getOptionValue("i");
+                LOGGER.log(Level.INFO, "Using input map file '" + inputMapFile + "'.");
+                // TODO: do something with this info: add to our app settings.
+            } else {
+                LOGGER.log(Level.SEVERE, "Missing required i option");
+                help(1);
+            }
+
+        } catch (ParseException e) {
+            LOGGER.log(Level.SEVERE, "Failed to parse command line arguments.", e);
+            help(1);
+        }
+    }
+
+    private void help(int retVal) {
+        HelpFormatter formater = new HelpFormatter();
+        formater.printHelp("qmapc", options);
+        System.exit(retVal);
+    }
+}
